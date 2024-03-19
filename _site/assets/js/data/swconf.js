066a00582b3a204d43c255ffb8621d0d0d750bc0
devforest1 <devforest01@gmail.com>
2024-03-19 15:21:37 +0900

    fix: config.yml 파일 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1710828266',resources: [
+    cacheName: 'chirpy-1710829284',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
