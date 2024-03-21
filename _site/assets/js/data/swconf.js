a174b1ef2289e06205d4def50042ccdc50902d13
devforest1 <devforest01@gmail.com>
2024-03-21 17:12:11 +0900

    fix: 블로그 포스팅 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1711007890',resources: [
+    cacheName: 'chirpy-1711008704',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
